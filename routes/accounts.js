229496d47814853c7e96e5bc5092db63ec4e7f90
Zack Claar <claar.zack@gmail.com>
2015-08-10 10:27:26 -0600

    New account creation page post is redirecting back to the account index page

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/accounts/', function(req, res, next) {
+  res.render('accounts/');
+});
+
 router.get('/accounts/login', function(req, res,next) {
   res.render('accounts/login');
+});
+
+router.get('/accounts/newAccount', function(req, res, next) {
+  res.render('accounts/newAccount');
+});
+
+router.post('/accounts/', function(req, res, next){
+  res.redirect('/accounts/');
 });
 
 module.exports = router;
