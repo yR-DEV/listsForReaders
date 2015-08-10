145fa34b1bc3b30101994405ee9398e52fd3f2f7
Zack Claar <claar.zack@gmail.com>
2015-08-10 10:48:55 -0600

    New Account page successfully posting to local mongodb

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/readingList');
+var accountCollection = db.get('accounts');
 
+//************* GET HOME PAGE ****************
 router.get('/accounts/', function(req, res, next) {
-  res.render('accounts/');
+  res.render('accounts/index');
 });
 
+//************* GET LOGIN PAGE ******************
 router.get('/accounts/login', function(req, res,next) {
   res.render('accounts/login');
 });
 
+//************ CREATE NEW ACC *******************
 router.get('/accounts/newAccount', function(req, res, next) {
   res.render('accounts/newAccount');
 });
 
+//*********** POST NEW ACCOUNT ******************
 router.post('/accounts/', function(req, res, next){
+
+  accountCollection.insert({username: req.body.newUserAccountName,
+                            password: req.body.newUserPassword1})
   res.redirect('/accounts/');
 });
 
