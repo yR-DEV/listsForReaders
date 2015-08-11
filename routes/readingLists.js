bb96f9d162b400e16c963ef31befb6de1b63d01d
Zack Claar <claar.zack@gmail.com>
2015-08-10 18:44:48 -0600

    Stuck, going to get help tomorrow

--- routes/readingLists.js
+++ routes/readingLists.js
@@ -5,21 +5,29 @@ require('dotenv').load();
 var goodreads = require('goodreads');
 var gr = new goodreads.client({'key': process.env.GOODREADS_DEV_KEY,
                                'secret': process.env.GOODREADS_DEV_SECRET});
+var grURL = "https://www.goodreads.com/search.xml?key=";
 
 //*********** GET READING LIST INDEX PAGE *******************
 router.get('/readingLists/', function(req, res, next) {
-  res.render('readingLists/');
+    res.render('readingLists/');
 });
 
 //********* GET NEW READING LIST PAGE ***********************
 router.get('/readingLists/newList', function(req, res, next) {
-  res.render('readingLists/newList');
+
+  var title = req.body.bookTitleSearch;
+
+  unirest.get("https://www.goodreads.com/api/author_url/Orson%20Scott%20Card?key=" + process.env.GOODREADS_DEV_KEY)
+  .end(function(response) {
+    console.log(response.body.author_id);
+    res.render('readingLists/newList', {response: response.body.author_id});
+  });
 });
 
 //******** NEW LIST REPOST, API CALLS IN HERE ***************
 router.post('/readingLists/newList', function(req, res, next) {
-  var title = req.body.bookTitleSearch;
-  res.render('readingLists/newList', {title: title});
+  // unirest.get(grURL + process.env.GOODREADS_DEV_KEY + '&q=Ender%27s+Game')
+    res.render('readingLists/newList');
 });
 
 module.exports = router;
